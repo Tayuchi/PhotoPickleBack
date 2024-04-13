c3c5ed5ea94b762050fa763c7249116e9603d9c4
Tayu <yukitake844@gmail.com>
2024-04-13 15:12:17 +0900

    mail settings

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,14 @@
+import os
+import environ
 from pathlib import Path
+from datetime import timedelta
+from decouple import config
+# from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -13,8 +19,7 @@ SECRET_KEY = 'django-insecure-5uqv4mi_u+o%*u@jj&icp-xhs_cv(enwp_4xn64c&94_h48f7=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -27,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "debug_toolbar",
     "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
     "corsheaders",
     "accounts",
     "map",
@@ -43,6 +50,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
+
+# CORS
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = []
 
 ROOT_URLCONF = 'config.urls'
 
@@ -75,6 +86,9 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -111,16 +125,84 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# メール設定
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+
+# REST_FRAMEWORK
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DATETIME_FORMAT": "%Y-%m-%d %H:%M",
 }
+
+# JWT
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
+    "AUTH_HEADER_TYPES": ("JWT",),
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+}
+
+# Djoser
+DJOSER = {
+    # メールアドレスでログイン
+    "LOGIN_FIELD": "email",
+    # アカウント本登録メール
+    "SEND_ACTIVATION_EMAIL": True,
+    # アカウント本登録完了メール
+    "SEND_CONFIRMATION_EMAIL": True,
+    # メールアドレス変更完了メール
+    "USERNAME_CHANGED_EMAIL_CONFIRMATION": True,
+    # パスワード変更完了メール
+    "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
+    # アカウント登録時に確認用パスワード必須
+    "USER_CREATE_PASSWORD_RETYPE": True,
+    # メールアドレス変更時に確認用メールアドレス必須
+    "SET_USERNAME_RETYPE": True,
+    # パスワード変更時に確認用パスワード必須
+    "SET_PASSWORD_RETYPE": True,
+    # アカウント本登録用URL
+    "ACTIVATION_URL": "signup/{uid}/{token}",
+    # パスワードリセット完了用URL
+    "PASSWORD_RESET_CONFIRM_URL": "reset-password/{uid}/{token}",
+    # カスタムユーザー用シリアライザー
+    "SERIALIZERS": {
+        "user_create": "accounts.serializers.UserSerializer",
+        "user": "accounts.serializers.UserSerializer",
+        "current_user": "accounts.serializers.UserSerializer",
+    },
+    "EMAIL": {
+        # アカウント本登録
+        "activation": "accounts.email.ActivationEmail",
+        # アカウント本登録完了
+        "confirmation": "accounts.email.ConfirmationEmail",
+        # パスワード再設定
+        "password_reset": "accounts.email.ForgotPasswordEmail",
+        # パスワード再設定確認
+        "password_changed_confirmation": "accounts.email.ResetPasswordEmail",
+    },
+}
+
+AUTH_USER_MODEL = "accounts.UserAccount"
+
+SITE_DOMAIN = env("SITE_DOMAIN")
+SITE_NAME = env("SITE_NAME")
